eba5f601c44606241ad467518c1388294e4d9b1f
fatahrez <fertahrez@gmail.com>
2018-10-13 17:04:12 +0300

    try connecting database sql2o

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -1,52 +1,81 @@
-import java.util.ArrayList;
 import java.util.List;
+import java.util.ArrayList;
+import org.sql2o.*;
 
 public class Client {
     private String name;
     private Integer age;
-    private String appearance;
+    private String firstappearance;
     private String neighbourhood;
-    public int id;
-    private static List<Client> instances = new ArrayList<Client>();
+    private int id;
 
     public Client(String name, Integer age, String firstAppearance, String neighbourhood){
-        mName =name;
-        mAge = age;
-        mFirstAppearance = firstAppearance;
-        mNeighbourhood = neighbourhood;
-        instances.add(this);
-        mId = instances.size();
-    }
+        this.name =name;
+        this.age = age;
+        this.firstappearance = firstAppearance;
+        this.neighbourhood = neighbourhood;
 
-    public static List<Client> all(){
-
-        return instances;
     }
 
     public int getId(){
 
-        return mId;
-    }
-
-    public static Client find(int id){
-
-        return instances.get(id - 1);
+        return id;
     }
 
     public String getName(){
 
-        return mName;
+        return name;
     }
     public Integer getAge(){
 
-        return mAge;
+        return age;
     }
     public String getFirstAppearance(){
 
-        return mFirstAppearance;
+        return firstappearance;
     }
     public String getNeighbourhood(){
 
-        return mNeighbourhood;
+        return neighbourhood;
+    }
+
+    public static List<Client> all() {
+        String sql = "SELECT id, name, firstappearance FROM clients";
+           try(Connection con = DB.sql2o.open()) {
+           return con.createQuery(sql).executeAndFetch(Client.class);
+        }
+     }
+
+    // public static Client find(int id){
+    //     try(Connection con = DB.sql2o.open()){
+    //         String sql = "SELECT * FROM clients where id =:id";
+    //         Client client = con.createQuery(sql)
+    //         .addParameter("id", id)
+    //         .executeAndFetchFirst(Client.class);
+    //         return client;
+    //     }
+    // }
+
+    public static Client find(int id) {
+        try(Connection con = DB.sql2o.open()) {
+           String sql = "SELECT * FROM clients where id=:id";
+           Client client = con.createQuery(sql)
+           .addParameter("id", id)
+           .executeAndFetchFirst(Client.class);
+           return client;
+        }
+     }
+
+    public void save(){
+        try(Connection con = DB.sql2o.open()){
+            String sql = "INSERT INTO clients(name, age, firstappearance, neighbourhood) VALUES (:name, :age, :firstappearance, :neighbourhood)";
+            this.id = (int) con.createQuery(sql, true)
+            .addParameter("name", this.name)
+            .addParameter("age", this.age)
+            .addParameter("firstappearance", this.firstappearance)
+            .addParameter("neighbourhood", this.neighbourhood)
+            .executeUpdate()
+            .getKey();
+        }
     }
 }
--- src/main/java/DB.java
+++ src/main/java/DB.java
@@ -1,5 +1,4 @@
 import org.sql2o.*;
-
 public class DB {
     public static Sql2o sql2o = new Sql2o("jdbc:postgresql://localhost:5432/hair_salon", "fatah", "6674fatah");
 }
