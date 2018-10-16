601e9b1da32a8c7cfc025f00708a32393218be5f
fatahrez <fertahrez@gmail.com>
2018-10-16 23:57:33 +0300

    Add properties for stylist inject them into constructor in Stylist.java

--- src/main/java/Stylist.java
+++ src/main/java/Stylist.java
@@ -1,2 +1,12 @@
 public class Stylist {
+  private int id;
+  private String name;
+  private String style;
+
+  public Stylist(String name, String style){
+    this.name = name;
+    this.style = style; 
+  }
+
+  
 }
