ed11551f0637e0e07c8d3337ee55e5f0a52ec5be
fatahrez <fertahrez@gmail.com>
2018-10-17 00:10:13 +0300

    Add functions to get all stylists and clients from stylist in Stylist.java

--- src/main/java/Stylist.java
+++ src/main/java/Stylist.java
@@ -1,3 +1,5 @@
+import java.util.List;
+import org.sql2o.Connection;;
 public class Stylist {
   private int id;
   private String name;
@@ -6,6 +8,31 @@ public class Stylist {
   public Stylist(String name, String style){
     this.name = name;
     this.style = style; 
+  }
+
+  public String getStylistName(){
+    return name;
+  }
+  public String style(){
+    return style;
+  }
+  public int getId(){
+    return id;
+  }
+
+  public static List<Stylist> allStylist(){
+    String sql = "SELECT * FROM stylist;";
+    try(Connection con = DB.sql2o.open()){
+      return con.createQuery(sql).executeAndFetch(Stylist.class);
+    }
+  } 
+  public List<Client> getClients(){
+    try(Connection con = DB.sql2o.open()){
+      String sql = "SELECT * FROM clients where stylistid=:id;";
+      return con.createQuery(sql)
+      .addParameter("id", this.id)
+      .executeAndFetch(Client.class);
+    }
   }
 
   
