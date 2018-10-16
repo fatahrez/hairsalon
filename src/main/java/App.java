cad97fb567c63d4103304fc097cda199f2284644
fatahrez <fertahrez@gmail.com>
2018-10-17 01:09:17 +0300

    add layout.vtl template to work as a parent of all templates

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,2 +1,42 @@
+import static spark.Spark.*;
+import spark.template.velocity.VelocityTemplateEngine;
+import spark.ModelAndView;
+import static spark.debug.DebugScreen.enableDebugScreen;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.eclipse.jetty.http.MetaData.Request;
+
 public class App {
+  public static void main(String[] args) {
+    staticFileLocation("/public"); 
+    String layout = "templates/layout.vtl";
+    enableDebugScreen();
+
+    get("/", (request, response)->{
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("stylists", Stylist.allStylist());
+      model.put("template", "templates/index.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/clients/new",(request, response)->{
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("stylists", Stylist.allStylist());
+      model.put("template", "templates/index.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    post("/stylist", (request, response)->{
+      Map<String, Object> model = new HashMap<String, Object>();
+      String name = request.queryParams("name");
+      String style = request.queryParams("style");
+      Stylist newStylist = new Stylist(name, style);
+      newStylist.save();
+      model.put("template", "templates/stylist.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+  }
 }
--- src/test/java/ClientTest.java
+++ src/test/java/ClientTest.java
@@ -7,45 +7,30 @@ import org.sql2o.Sql2o;
 public class ClientTest {
     @Before
     public void setUp() {
-        DB.sql2o = new Sql2o("jdbc:postgresql://localhost:5432/hair_salon_test", "fatah", "6674fatah");
+        DB.sql2o = new Sql2o("jdbc:postgresql://localhost:5432/my_salon_test", "fatah", "6674fatah");
     }
     @After
     public void tearDown(){
         try(Connection con = DB.sql2o.open()){
-            String sql = "DELETE FROM client *;";
+            String sql = "DELETE FROM clients *;";
             con.createQuery(sql).executeUpdate();
         }
     }
     @Test
     public void clientReturnsAnInstanceOfObject_object(){
-        Client client = new Client("shiro", 20, "may", "koma");
+        Client client = new Client("hll","hjjn",1);
         assertTrue(client instanceof Object);
     }
     @Test
     public void getNameOfClient_String(){
-        Client client = new Client("shiro", 20, "may", "koma");
-        assertEquals("shiro", client.getName());
+        Client client = new Client("hll","hjjn",1);
+        assertEquals("hll", client.getClientName());
     }
-    @Test
-    public void getAgeOfClient_int(){
-        Client client = new Client("shiro", 20, "may", "koma");
-        Integer age = 20;
-        assertEquals(age, client.getAge());
-    }
-    @Test
-    public void getFirstApperanceofClient_string(){
-        Client client = new Client("shiro", 20, "may", "koma");
-        assertEquals("may", client.getFirstAppearance());
-    }
-    @Test
-    public void getneigghbourhood_string(){
-        Client client = new Client("shiro", 20, "may", "koma");
-        assertEquals("koma", client.getNeighbourhood());
-    }
+
     @Test
     public void returnsClientsAsInstanceOfClientClass_Client(){
-        Client client = new Client("shiro", 20, "may", "koma");
-        Client client2 = new Client("epha", 20, "may", "koma");
+        Client client = new Client("hll","hjjn",1);
+        Client client2 = new Client("dhkj","dhjhj",1);
 
         assertTrue(Client.all().contains(client));
         assertTrue(Client.all().contains(client2));
